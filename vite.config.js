adde6a064d895008f1238811f2e706ae54aa40fd
CodeNest <aleksander.potyka@gmail.com>
2025-04-30 16:13:58 +0200

    Fix Vercel paths: base set to root

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bistro-landing-page/',
+  base: '/',
   plugins: [react()],
 });
